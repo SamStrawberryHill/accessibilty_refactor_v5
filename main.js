2cb6841083007ea0a30bb98c7d46a2b137d4ddbc
Samantha Hill <samhill5643@gmail.com>
2016-05-26 18:20:33 -0400

    Form Update

---
+++ main.js
@@ -0,0 +1,25 @@
+function sortlist()
+{
+    var albums = document.getElementById('albums');
+    var albumTexts = new Array();
+
+    for(i = 2; i < albums.length; i++)
+    {
+        albumTexts[i-2] =
+            albums.options[i].text.toUpperCase() + "," +
+            albums.options[i].text + "," +
+            albums.options[i].value;
+    }
+
+    albumTexts.sort();
+
+    for(i = 2; i < albums.length; i++)
+    {
+        var parts = albumTexts[i-2].split(',');
+
+        albums.options[i].text = parts[1];
+        albums.options[i].value = parts[2];
+    }
+}
+
+sortlist();
